1b1a4f736321870bd0afe9663a5a297397640d90
Javad Adib <Mr.Javad.Adib@gmail.com>
2020-06-12 15:55:41 +0430

    fix default namespace

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace jibres-booster-v2.Properties {
+namespace JibresBooster.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace jibres-booster-v2.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("jibres-booster-v2.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("JibresBooster.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
